a2b74d7717bf8cc65aff469f1ae2240132400b41
martin <martin.id@kakaopaycorp.com>
2021-03-17 20:06:39 +0900

    ch9. query-method-practice-1

--- src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.fastcampus.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.domain.User;
+import java.util.List;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -8,4 +10,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @since 2021/03/10
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+    Set<User> findByName(String name);
+
+    User findByEmail(String email);
+
+    User getByEmail(String email);
+
+    User readByEmail(String email);
+
+    User queryByEmail(String email);
+
+    User searchByEmail(String email);
+
+    User streamByEmail(String email);
+
+    User findUserByEmail(String email);
+
+    User findSomethingByEmail(String email);
+
+    List<User> findFirst2ByName(String name);
+
+    List<User> findTop2ByName(String name);
+
+    List<User> findLast1ByName(String name);
 }
--- src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -23,4 +23,23 @@ class UserRepositoryTest {
 
         userRepository.save(user);
     }
+
+    @Test
+    void select() {
+        System.out.println(userRepository.findByName("dennis"));
+
+        System.out.println("findByEmail : " + userRepository.findByEmail("martin@fastcampus.com"));
+        System.out.println("getByEmail : " + userRepository.getByEmail("martin@fastcampus.com"));
+        System.out.println("readByEmail : " + userRepository.readByEmail("martin@fastcampus.com"));
+        System.out.println("queryByEmail : " + userRepository.queryByEmail("martin@fastcampus.com"));
+        System.out.println("searchByEmail : " + userRepository.searchByEmail("martin@fastcampus.com"));
+        System.out.println("streamByEmail : " + userRepository.streamByEmail("martin@fastcampus.com"));
+        System.out.println("findUserByEmail : " + userRepository.findUserByEmail("martin@fastcampus.com"));
+
+        System.out.println("findSomethingByEmail : " + userRepository.findSomethingByEmail("martin@fastcampus.com"));
+
+        System.out.println("findTop2ByName : " + userRepository.findTop2ByName("martin"));
+        System.out.println("findFirst2ByName : " + userRepository.findFirst2ByName("martin"));
+        System.out.println("findLast1ByName : " + userRepository.findLast1ByName("martin"));
+    }
 }
