e326b2f0d9a65dcf1f1e1a2c80064e97fc1d5850
martin <martin.id@kakaopaycorp.com>
2021-05-13 21:09:16 +0900

    ch20. entity cache

---
+++ src/test/java/com/fastcampus/jpa/bookmanager/service/EntityManagerTest.java
@@ -0,0 +1,53 @@
+package com.fastcampus.jpa.bookmanager.service;
+
+import com.fastcampus.jpa.bookmanager.domain.User;
+import com.fastcampus.jpa.bookmanager.repository.UserRepository;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Martin
+ * @since 2021/05/13
+ */
+@SpringBootTest
+@Transactional
+public class EntityManagerTest {
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void entityManagerTest() {
+        System.out.println(entityManager.createQuery("select u from User u").getResultList());
+    }
+
+    @Test
+    void cacheFindTest() {
+//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+//        System.out.println(userRepository.findById(2L).get());
+//        System.out.println(userRepository.findById(2L).get());
+//        System.out.println(userRepository.findById(2L).get());
+//
+        userRepository.deleteById(1L);
+    }
+
+    @Test
+    void cacheFindTest2() {
+        User user = userRepository.findById(1L).get();
+        user.setName("marrrrrrrtin");
+        userRepository.save(user);
+
+        System.out.println("---------------------");
+
+        user.setEmail("marrrrrrtin@fastcampus.com");
+        userRepository.save(user);
+
+        System.out.println(userRepository.findAll());       // select * from user
+    }
+}
